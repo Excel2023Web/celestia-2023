1dc4450f19248107c8830b793e6f7c651de85ec7
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-09-23 23:24:16 +0530

    css

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import "./MainPage.css";
-import { Navbar, Timeline, Panelist, Speakers, Contacts, About } from "../../components";
+import {
+  Navbar,
+  Timeline,
+  Panelist,
+  Speakers,
+  Contacts,
+  About,
+  Footer,
+} from "../../components";
 
 const MainPage = () => {
   return (
     <>
       <div className="main__page">
-        {/* <Stars/> */}
         <Navbar />
-        <About/>
+        <About />
         <Speakers />
         <Panelist />
         <Timeline />
         <Contacts />
+        <Footer/>
       </div>
     </>
   );
