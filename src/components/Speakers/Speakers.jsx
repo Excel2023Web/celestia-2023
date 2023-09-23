1fc87576d8076246410fc60721efa953ba353db0
nandiinii <preethanandini175@gmail.com>
2023-09-23 23:26:01 +0530

    contact query partially done

--- src/components/Speakers/Speakers.jsx
+++ src/components/Speakers/Speakers.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './Speakers.css';
 import heading from "../../assets/svg/speakers_heading.svg";
 import verticalLine from '../../assets/svg/spkr_line.svg';
-import { FaLinkedinIn } from "react-icons/fa6";
+// import { FaLinkedinIn } from "react-icons/fa6";
 import { speakersData } from '../../assets/data/speaker';
 
 const Speakers = () => {
   return (
     <div className='speakers__main' id='speakers'>
       <div className='panelist__container'>
-        {/* <p className='panelist__heading' data-aos="zoom-in">Speakers</p> */}
+        {/* <p className='panelist__heading' data-aos="zoom-in"> <FaLinkedinIn className='linkedin__icon' color='#071C44'/> Speakers</p> */}
         <img className='speaker__heading' src={heading} alt=" "></img> 
       </div>
       <div className="spkrs__container">
@@ -21,7 +21,7 @@ const Speakers = () => {
                 <div className="spkr__context">
                     <p>{contact.name}</p>
                     <button className='speaker__contact'><a href={contact.linkedIn} target="_blank"
-                                rel="noreferrer"> <FaLinkedinIn className='linkedin__icon' color='#071C44'/> CONNECT </a></button>
+                                rel="noreferrer"> CONNECT </a></button>
                 </div>
             </div>
         </div>
