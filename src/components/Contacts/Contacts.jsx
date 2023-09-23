9976e94908d9bed1d7f87009d6f1118dc896a495
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-09-23 13:20:53 +0530

    contacts data

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,150 +1,68 @@
-import React from 'react';
-import './Contacts.css';
+import React from "react";
+import "./Contacts.css";
 import Line from "../../assets/png/header_line.png";
-import { MdLocalPhone,MdEmail } from "react-icons/md";
+import { MdLocalPhone, MdEmail } from "react-icons/md";
 import { FaLinkedinIn } from "react-icons/fa";
-
+import { contacts } from "../../data/contacts";
 const Contacts = () => {
   return (
     <>
-        <div className='contact__out_container' id='contact'>
-            <div className='panelist__container'>
-                <p className='panelist__heading' data-aos="fade-up">Contacts</p>
-                <img className='header__line' src={Line} alt=" " data-aos="fade-up"></img> 
-            </div>
-            <div className='contacts__container'>
-                <div className="contact__card" data-aos="fade-up">
-                    <img src="https://avatars.githubusercontent.com/u/110590339?s=200&v=4" alt=" " />
-                    <div className="contact__content">
-                        <div className="contact__context">
-                            <h1>Mathew</h1>
-                            <p>Chairperson</p>
-                            <div className='icons__container'>
-                                <a 
-                                target="_blank"
-                                rel="noreferrer"
-                                href='https://www.linkedin.com/in/nandini-p-8388b3250/'
-                                >
-                                    <MdEmail />
-                                </a>
-                                <a 
-                                target="_blank"
-                                rel="noreferrer"
-                                href='https://www.linkedin.com/in/nandini-p-8388b3250/'
-                                >
-                                    <MdLocalPhone />
-                                </a>
-                                <a 
-                                target="_blank"
-                                rel="noreferrer"
-                                href='https://www.linkedin.com/in/nandini-p-8388b3250/'
-                                >
-                                    <FaLinkedinIn />
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="contact__card" data-aos="fade-up">
-                    <img src="https://avatars.githubusercontent.com/u/110590339?s=200&v=4" alt=" " />
-                    <div className="contact__content">
-                        <div className="contact__context">
-                            <h1>Mathew</h1>
-                            <p>Designation</p>
-                            <div className='icons__container'>
-                                <a 
-                                target="_blank"
-                                rel="noreferrer"
-                                href='https://www.linkedin.com/in/nandini-p-8388b3250/'
-                                >
-                                    <MdEmail />
-                                </a>
-                                <a 
-                                target="_blank"
-                                rel="noreferrer"
-                                href='https://www.linkedin.com/in/nandini-p-8388b3250/'
-                                >
-                                    <MdLocalPhone />
-                                </a>
-                                <a 
-                                target="_blank"
-                                rel="noreferrer"
-                                href='https://www.linkedin.com/in/nandini-p-8388b3250/'
-                                >
-                                    <FaLinkedinIn />
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="contact__card" data-aos="fade-up">
-                    <img src="https://avatars.githubusercontent.com/u/110590339?s=200&v=4" alt=" " />
-                    <div className="contact__content">
-                        <div className="contact__context">
-                            <h1>Mathew</h1>
-                            <p>Designation</p>
-                            <div className='icons__container'>
-                                <a 
-                                target="_blank"
-                                rel="noreferrer"
-                                href='https://www.linkedin.com/in/nandini-p-8388b3250/'
-                                >
-                                    <MdEmail />
-                                </a>
-                                <a 
-                                target="_blank"
-                                rel="noreferrer"
-                                href='https://www.linkedin.com/in/nandini-p-8388b3250/'
-                                >
-                                    <MdLocalPhone />
-                                </a>
-                                <a 
-                                target="_blank"
-                                rel="noreferrer"
-                                href='https://www.linkedin.com/in/nandini-p-8388b3250/'
-                                >
-                                    <FaLinkedinIn />
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="contact__card" data-aos="fade-up">
-                    <img src="https://avatars.githubusercontent.com/u/110590339?s=200&v=4" alt=" " />
-                    <div className="contact__content">
-                        <div className="contact__context">
-                            <h1>Mathew</h1>
-                            <p>Designation</p>
-                            <div className='icons__container'>
-                                <a 
-                                target="_blank"
-                                rel="noreferrer"
-                                href='https://www.linkedin.com/in/nandini-p-8388b3250/'
-                                >
-                                    <MdEmail />
-                                </a>
-                                <a 
-                                target="_blank"
-                                rel="noreferrer"
-                                href='https://www.linkedin.com/in/nandini-p-8388b3250/'
-                                >
-                                    <MdLocalPhone />
-                                </a>
-                                <a 
-                                target="_blank"
-                                rel="noreferrer"
-                                href='https://www.linkedin.com/in/nandini-p-8388b3250/'
-                                >
-                                    <FaLinkedinIn />
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+      <div className="contact__out_container" id="contact">
+        <div className="panelist__container">
+          <p className="panelist__heading" data-aos="fade-up">
+            Contacts
+          </p>
+          <img
+            className="header__line"
+            src={Line}
+            alt=" "
+            data-aos="fade-up"
+          ></img>
         </div>
+        <div className="contacts__container">
+          {contacts.map((contact, index) => {
+            return (
+              <div className="contact__card" data-aos="fade-up" key={index}>
+                <img
+                  src={contact.image}
+                  alt=" "
+                />
+                <div className="contact__content">
+                  <div className="contact__context">
+                    <h1>{contact.name}</h1>
+                    <p>{contact.position}</p>
+                    <div className="icons__container">
+                      <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href={contact.email}
+                      >
+                        <MdEmail />
+                      </a>
+                      <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href={contact.phno}
+                      >
+                        <MdLocalPhone />
+                      </a>
+                      <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href={contact.linkedin}
+                      >
+                        <FaLinkedinIn />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Contacts
+export default Contacts;
---
+++ src/data/contacts.js
@@ -0,0 +1,59 @@
+export const contacts=[
+    {
+        name:"Mathew Davis",
+        position:"Chairperson",
+        linkedin:"",
+        email:"",
+        phno:"",
+        image:"https://avatars.githubusercontent.com/u/110590339?s=200&v=4"
+    },
+    {
+        name:"Vishnu S",
+        position:"General Secretary",
+        linkedin:"",
+        email:"",
+        phno:"",
+        image:"https://avatars.githubusercontent.com/u/110590339?s=200&v=4"
+    },
+    {
+        name:"Abhishek P",
+        position:"Joint Secretary",
+        linkedin:"",
+        email:"",
+        phno:"",
+        image:"https://avatars.githubusercontent.com/u/110590339?s=200&v=4"
+    },
+    {
+        name:"Faiza S Rahman",
+        position:"Conference and Workshops Manager",
+        linkedin:"",
+        email:"",
+        phno:"",
+        image:"https://avatars.githubusercontent.com/u/110590339?s=200&v=4"
+    },
+    {
+        name:"Vaishnav S",
+        position:"Conference and Workshops Manager(PC)",
+        linkedin:"",
+        email:"",
+        phno:"",
+        image:"https://avatars.githubusercontent.com/u/110590339?s=200&v=4"
+    },
+    {
+        name:"Mathew Davis",
+        position:"Event Head",
+        linkedin:"",
+        email:"",
+        phno:"",
+        image:"https://avatars.githubusercontent.com/u/110590339?s=200&v=4"
+    },
+    {
+        name:"Mathew Davis",
+        position:"Event Head",
+        linkedin:"",
+        email:"",
+        phno:"",
+        image:"https://avatars.githubusercontent.com/u/110590339?s=200&v=4"
+    },
+
+]
