fddadfa51bf47b4aaff0022b0c17c839a4027afe
jzf21 <jozefneelamkavil2003@gmail.com>
2023-09-23 20:11:47 +0530

    new

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React from "react";
 import "./Landing.css";
+import image from "../../assets/svg/astronaut-is-flying-space-suit_219493-15-removebg-preview 1.svg";
+import { motion } from "framer-motion";
 
 const Landing = () => {
   return (
     <>
-    <div id='home'>Landing</div>
+      <div id="home" className="landing_container">
+        {/* <motion.img
+          // initial={{ x: "-200%", y: 0 }}
+          //   animate={{ x: "-100%", y: 0}}
+          transition={{
+            opacity: { ease: "linear" },
+            layout: { duration: 0.3 },
+            delay: 0.2,
+          }}
+          src={image}
+          alt="astronaut"
+          className="landing_image"
+        /> */}
+        <div className="landing_text">Celestia</div>
+        <div className="mid_container">
+          <div className="landing_text2 row-container">
+            <p className="landing_subheading">IMA Hall, Kaloor</p>
+            <p className="landing_subheading">6 November 2022 </p>
+            <p className="landing_subheading">10:00 AM</p>
+          </div>
+          <button className="landing_button">Register Now</button>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./MainPage.css";
-import { Navbar, Timeline, Panelist, Speakers, Contacts } from "../../components";
+import { Navbar, Timeline, Panelist, Speakers, Contacts, Landing } from "../../components";
 
 const MainPage = () => {
   return (
     <>
       <div className="main__page">
         <Navbar />
+        <Landing/>
         <Speakers />
         <Panelist />
         <Timeline />
