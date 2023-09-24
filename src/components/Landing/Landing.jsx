fc58c2a24c63ee3fdafe7cad6a453405404fd65d
jzf21 <jozefneelamkavil2003@gmail.com>
2023-09-24 21:32:58 +0530

    responsive changes

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Landing.css";
 import { useSpring, animated } from "react-spring";
 // import { duration } from "@mui/material";
-import { Link } from "react-scroll";
+// import { Link } from "react-scroll";
 const Landing = () => {
   const landingTextAnimation = useSpring({
     from: { opacity: 0, transform: "translateY(-20px)" },
@@ -70,13 +70,13 @@ const Landing = () => {
         <animated.button className="landing_button" style={buttonAnimation}>
           Register Now
         </animated.button>
-        <section id="section07" className="demo">
+        {/* <section id="section07" className="demo">
           <Link to="about" style={{ cursor: "pointer" }}>
             <span></span>
             <span></span>
             <span></span>
           </Link>
-        </section>
+        </section> */}
       </div>
     </div>
   );
