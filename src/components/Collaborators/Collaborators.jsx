2c068516b80f1348b1fe4b6b52522a148c2a0a18
nandiinii <preethanandini175@gmail.com>
2023-09-27 19:01:13 +0530

    css changes

--- src/components/Collaborators/Collaborators.jsx
+++ src/components/Collaborators/Collaborators.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Collaborators.css";
-import { ukexpress,ksum,bosch,GulfLogo,careerlauncher} from '../../assets/gallery/collaborators';
+import { careerlauncher } from '../../assets/gallery/collaborators';
 
 const Collaborators = () => {
   return (
@@ -9,18 +9,6 @@ const Collaborators = () => {
           Collaborators
         </div>
         <div className='collab__images'> 
-        <div
-            data-aos="zoom-in"
-            data-aos-delay="150"
-            className="collab-section"
-          >
-            <img
-              src={ukexpress}
-              alt=""
-              className="c-images"
-            ></img>
-            <p>UK Xpress</p>
-          </div>
           <div
             data-aos="zoom-in"
             data-aos-delay="200"
@@ -32,34 +20,6 @@ const Collaborators = () => {
               className="c-images"
             ></img>
             <p>Career Launcher</p>
-          </div>
-          <div
-            data-aos="zoom-in"
-            data-aos-delay="250"
-            className="collab-section"
-          >
-            <img src={GulfLogo} alt="" className="c-images"></img>
-            <p>Gulf</p>
-          </div>
-          <div
-            data-aos="zoom-in"
-            data-aos-delay="300"
-            className="collab-section"
-          >
-            <img src={bosch} alt="" className="c-images"></img>
-            <p>Bosch</p>
-          </div>
-          <div
-            data-aos="zoom-in"
-            data-aos-delay="150"
-            className="collab-section"
-          >
-            <img
-              src={ksum}
-              alt=""
-              className="c-images"
-            ></img>
-            <p>KSUM</p>
           </div>
         </div>
     </div>
