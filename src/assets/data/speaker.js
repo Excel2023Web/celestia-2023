9cb240d63cfcea4b4812ed94cda86a9eaa8b7c05
nandiinii <preethanandini175@gmail.com>
2023-10-02 22:49:13 +0530

    css

--- src/assets/data/speaker.js
+++ src/assets/data/speaker.js
@@ -1,4 +1,4 @@
-import img from "../png/speaker_img.png";
+// import img from "../png/speaker_img.png";
 import { vennila, sundararajan } from "../gallery/speakers";
 // import { sundararajan } from "../jpg";
 export const speakersData = [
@@ -16,20 +16,20 @@ export const speakersData = [
     designation:"Manager, Process Quality(System in-house realisation), ISRO",
     linkedIn: "https://www.linkedin.com/in/sebin-davis-344782195",
     },
-    {
-    id: 3,
-    name: "Sebin Davis",
-    image: img,
-    designation:"Retired Associate Programme Director of Space Transportation Systems, VSSC",
-    linkedIn: "https://www.linkedin.com/in/sebin-davis-344782195",
-    },
-    {
-    id: 4,
-    name: "Sania Maria Sabu",
-    image: img,
-    designation:"Retired Associate Programme Director of Space Transportation Systems, VSSC",
-    linkedIn: "https://www.linkedin.com/in/sania-maria-sabu-1b059a1b2/",
-    },
+    // {
+    // id: 3,
+    // name: "Sebin Davis",
+    // image: img,
+    // designation:"Retired Associate Programme Director of Space Transportation Systems, VSSC",
+    // linkedIn: "https://www.linkedin.com/in/sebin-davis-344782195",
+    // },
+    // {
+    // id: 4,
+    // name: "Sania Maria Sabu",
+    // image: img,
+    // designation:"Retired Associate Programme Director of Space Transportation Systems, VSSC",
+    // linkedIn: "https://www.linkedin.com/in/sania-maria-sabu-1b059a1b2/",
+    // },
     // {
     // id: 5,
     // name: "Sebin Davis",
