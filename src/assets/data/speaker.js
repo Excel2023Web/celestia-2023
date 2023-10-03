59fd126367a5be9d7c62889b939482525958b9b6
nandiinii <preethanandini175@gmail.com>
2023-10-03 22:16:42 +0530

    speakers section added

--- src/assets/data/speaker.js
+++ src/assets/data/speaker.js
@@ -1,45 +1,39 @@
 // import img from "../png/speaker_img.png";
-import { vennila, sundararajan } from "../gallery/speakers";
-// import { sundararajan } from "../jpg";
+import { vennila, sundararajan, remiya, senan } from "../gallery/speakers";
 export const speakersData = [
     {
     id: 1,
-    name: "Dr. T Sundararajan ",
+    name: "Shri. T Sundararajan",
     image: sundararajan,
     designation:"Retired Associate Programme Director of Space Transportation Systems, VSSC",
     linkedIn: "https://www.linkedin.com/in/sebin-davis-344782195",
     },
     {
     id: 2,
-    name: "Vennila M",
+    name: "Smt. Vennila M",
     image: vennila,
-    designation:"Manager, Process Quality(System in-house realisation), ISRO",
+    designation:"Quality Assurance Manager, ISRO",
     linkedIn: "https://www.linkedin.com/in/sebin-davis-344782195",
     },
-    // {
-    // id: 3,
-    // name: "Sebin Davis",
-    // image: img,
-    // designation:"Retired Associate Programme Director of Space Transportation Systems, VSSC",
-    // linkedIn: "https://www.linkedin.com/in/sebin-davis-344782195",
-    // },
-    // {
-    // id: 4,
-    // name: "Sania Maria Sabu",
-    // image: img,
-    // designation:"Retired Associate Programme Director of Space Transportation Systems, VSSC",
-    // linkedIn: "https://www.linkedin.com/in/sania-maria-sabu-1b059a1b2/",
-    // },
+    {
+    id: 3,
+    name: "Shri. Rajeev Senan C",
+    image: senan,
+    designation:"Division Head, Monopropellant Systems & Components Division, LPSC/ISRO",
+    linkedIn: "https://www.linkedin.com/in/sebin-davis-344782195",
+    },
+    {
+    id: 4,
+    name: "Smt. Dr Remiya A.M",
+    image: remiya,
+    designation:"Associate Professor, Department of Earth and Space Sciences, IIST",
+    linkedIn: "https://www.linkedin.com/in/sania-maria-sabu-1b059a1b2/",
+    },
     // {
     // id: 5,
-    // name: "Sebin Davis",
+    // name: "Shri. Kiran Mohan",
     // image: img,
+    // designation:"Deputy Project Director, LPSC ISRO",
     // linkedIn: "https://www.linkedin.com/in/sebin-davis-344782195",
-    // },
-    // {
-    // id: 6,
-    // name: "Sania Maria Sabu",
-    // image: img,
-    // linkedIn: "https://www.linkedin.com/in/sania-maria-sabu-1b059a1b2/",
     // },
 ]
--- src/assets/gallery/speakers/index.js
+++ src/assets/gallery/speakers/index.js
@@ -1,2 +1,5 @@
 export { default as vennila } from "./vennila.png";
 export { default as sundararajan } from "./sundararajan.png";
+export { default as remiya } from "./remiya.png";
+export { default as senan } from "./senan.png";
+// export { default as sundararajan } from "./sundararajan.png";
